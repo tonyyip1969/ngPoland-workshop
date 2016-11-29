4cf8ec7826bc72c663830f0b341204d2f669e214
nir@500tech.com <nir@500tech.com>
2016-11-29 12:51:21 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {CustomValidators} from "./custom-validators";
 
 @Component({
   selector: 'app-login',
   template: `
-    <form class="login-form">
-      <div>
-          <input type="text" placeholder="username">
-      </div>
-      <div>
-          <input type="text" placeholder="password">
-      </div>
-      <button>Login</button>
+    <form [formGroup]="loginForm" 
+          (ngSubmit)="login()"
+          class="login-form">
+                              
+          <input type="text"
+                 name="username"                 
+                 formControlName="username"
+                 placeholder="username">
+                                                                                                                                              
+          <input type="password"
+                 name="password"
+                 formControlName="password"
+                 placeholder="password">
+                                  
+      <button type="submit">Login</button>
     </form>
   `,
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit() {
+  public loginForm: FormGroup;
+  public basicInfo: FormGroup;
+  public username: FormControl;
+  public password: FormControl;
+
+  constructor() {
+    this.username = new FormControl('', Validators.compose([
+      CustomValidators.noSpace,
+      CustomValidators.isNir
+    ]));
+
+    this.password = new FormControl('');
+
+    this.basicInfo = new FormGroup({
+      username: this.username,
+      password: this.password
+    })
+
+    this.loginForm = new FormGroup({
+      info: this.basicInfo
+    });
   }
 
+  public login() {
+    console.log(this.loginForm);
+  }
 }
