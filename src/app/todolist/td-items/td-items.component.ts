f14b92a979ae01e359448c7360ec7d84c1ef2808
nir@500tech.com <nir@500tech.com>
2016-11-30 09:35:20 +0200

    nir

--- src/app/todolist/td-items/td-items.component.ts
+++ src/app/todolist/td-items/td-items.component.ts
@@ -6,23 +6,15 @@ import { Http } from '@angular/http';
 @Component({
   selector: 'app-td-items',
   template: `  
-  <ul class="todo-list">
-    <app-td-item *ngFor="let item of items" [item]="item">
-     </app-td-item>                            
-  </ul>    
-  `  
+      <ul class="todo-list">
+        <app-td-item *ngFor="let item of list.items" [item]="item">
+        </app-td-item>                            
+      </ul>    
+     `  
 })
-export class TdItemsComponent  {
 
-  public items = [];
-
-  constructor(http:Http) {
-      
-      fetch('http://localhost:3000/items')          
-          .then( res => console.log(res.json()) )
-         
-  }
-
+export class TdItemsComponent  {  
+  constructor(private list: Todolist) {}
 }
 
 
